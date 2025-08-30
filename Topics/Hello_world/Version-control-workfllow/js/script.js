5a42ace16f3305aa18731d6b613f4e616fde8d02
Lazer6769 <Laser197@hotmail.com>
2025-08-30 16:01:24 -0400

    switched code with code that I made during my college semester change it so you can control the circle instead of the circle going back and forth on its own

--- Topics/Hello_world/Version-control-workfllow/js/script.js
+++ Topics/Hello_world/Version-control-workfllow/js/script.js
@@ -1,24 +1,26 @@
 /**
- * Title of Project
- * Author Name
+ * New year New "code"
+ * Jordan Lobasso 
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * this is something simple i made where you can just move a colourful circle between a black background and a square. 
+ * if you enter the square the trail from the circle fades out and if you leave the square the trail stays. 
  */
 
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+  createCanvas(600, 400);
+  background(0,255)
+  noCursor();
 }
-
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+  
+function draw() { 
+ stroke(random(255), random(255), random(255));
+strokeWeight(5);  
+fill(random(255), random(255), random(255));
+ellipse(mouseX, mouseY, 100, 100);
+  
+fill(0,0,0,10);
+  rect(150,50,300);
+  fill(random(255),random(255), random(255), random(255));
+       circle(random(700), random(400), random(50));
 
 }

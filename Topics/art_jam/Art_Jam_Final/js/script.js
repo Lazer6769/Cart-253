d8124306511aafedd8fc73c50652663c49e8729b
Lazer6769 <Laser197@hotmail.com>
2025-09-27 21:49:54 -0400

    added nose and slider for nose

--- Topics/art_jam/Art_Jam_Final/js/script.js
+++ Topics/art_jam/Art_Jam_Final/js/script.js
@@ -14,12 +14,37 @@ var reslider = 0;
 var grslider = 0;
 var blslider = 0;
 
+var reb;
+var gre;
+var blu;
+
+var rebslider = 0;
+var greslider = 0;
+var bluslider = 0;
+
+var ree;
+var grn;
+var bue;
+
+var reeslider = 0;
+var grneslider = 0;
+var bueslider = 0;
+
+
+
 
 function setup() {
     createCanvas(400, 400);
+    /* resetSketch();
+     
+     var button = createButton("reset");
+     button.mousePressed(resetSketch)
+     button.position(10,10);
+     */
 
     rectMode(CENTER);
     angleMode(DEGREES);
+
 
     rslider = createSlider(0, 255, 0, 5);
     gslider = createSlider(0, 255, 0, 5);
@@ -32,26 +57,50 @@ function setup() {
     blslider = createSlider(0, 255, 0, 5);
     blslider.position(265, 430);
 
+    rebslider = createSlider(0, 255, 0, 5);
+    rebslider.position(0, 460);
+    greslider = createSlider(0, 255, 0, 5);
+    greslider.position(135, 460);
+    bluslider = createSlider(0, 255, 0, 5);
+    bluslider.position(265, 460);
+
+    reeslider = createSlider(0, 255, 0, 5);
+    reeslider.position(0, 490);
+    grnslider = createSlider(0, 255, 0, 5);
+    grnslider.position(135, 490);
+    bueslider = createSlider(0, 255, 0, 5);
+    bueslider.position(265, 490);
+
+    //background(255);
+
+
+
 }
 
+
 function draw() {
-    background(220);
-    console.log(mouseX, mouseY);
+    //background(220);
+    // console.log(mouseX,mouseY);
+
     drawMe();
+
+
+
 }
 
 function drawMe() {
     noStroke();
 
     //body
-
     push();
-    fill(234, 153, 0);
+    var ree = reeslider.value();
+    var grn = grnslider.value();
+    var bue = bueslider.value();
+    fill(ree, grn, bue);
     ellipse(210, 380, 300, 200);
     pop();
 
     //head
-
     push();
     var r = rslider.value();
     var g = gslider.value();
@@ -70,7 +119,7 @@ function drawMe() {
     push();
     var re = reslider.value();
     var gr = grslider.value();
-    var bl = blslider.value()
+    var bl = blslider.value();
     fill(re, gr, bl);
     ellipse(155, 200, 45, 60);
     ellipse(265, 200, 45, 60);
@@ -84,8 +133,12 @@ function drawMe() {
     pop();
     //nose 
     push();
-    fill(243, 147, 84);
-    stroke(162, 96, 53);
+    var reb = rebslider.value();
+    var gre = greslider.value();
+    var blu = bluslider.value();
+    fill(reb, gre, blu);
+    noStroke();
     triangle(210, 225, 245, 265, 170, 265);
     pop();
 }
+

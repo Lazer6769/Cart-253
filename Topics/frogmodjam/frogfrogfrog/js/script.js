00fb5023cd590f733f45fcb86fa4930bdee5bde4
Lazer6769 <Laser197@hotmail.com>
2025-10-20 22:31:59 -0400

    in this I've added sounds, a picture of flames, changed the background, added more flies with different speeds, made the frog evil, and put everyone in hell for tonight's commit

--- Topics/frogmodjam/frogfrogfrog/js/script.js
+++ Topics/frogmodjam/frogfrogfrog/js/script.js
@@ -20,8 +20,30 @@ const frog = {
     // The frog's body has a position and size
     body: {
         x: 320,
-        y: 520,
-        size: 150
+        y: 480,
+        size: 200
+    },
+    Eye1: {
+        x: 280,
+        y: 440,
+        size: 75
+
+    },
+    Eye2: {
+        x: 360,
+        y: 440,
+        size: 75
+    },
+    Pupil1: {
+        x: 280,
+        y: 440,
+        size: 45
+    },
+
+    Pupil2: {
+        x: 360,
+        y: 440,
+        size: 45
     },
     // The frog's tongue has a position, size, speed, and state
     tongue: {
@@ -43,6 +65,47 @@ const fly = {
     speed: 3
 };
 
+const irregularfly = {
+    x: 0,
+    y: 150,
+    size: 10,
+    speed: 10
+};
+
+const slowfly = {
+    x: 0,
+    y: 100,
+    size: 10,
+    speed: 0.5
+}
+
+const sounds = {
+    buzzing: undefined,
+    gulp: undefined,
+    slurp: undefined,
+    beebuzzing: undefined,
+    scream: undefined,
+    otherscream: undefined,
+    scarymusic: undefined,
+    anotherscream: undefined,
+};
+
+
+function preload() {
+
+    sounds.scream = loadSound("assets/sounds/scream.mp3");
+    sounds.otherscream = loadSound("assets/sounds/luigi-burning.mp3");
+    sounds.anotherscream = loadSound("assets/sounds/hl1scream.mp3");
+    sounds.buzzing = loadSound("assets/sounds/mosquito.mp3");
+    sounds.beebuzzing = loadSound("assets/sounds/beebuzzing.mp3");
+    sounds.slurp = loadSound("assets/sounds/yoshi-tongue-sound-snes.mp3");
+    sounds.gulp = loadSound("assets/sounds/gulp-with-bubble.mp3");
+    sounds.scarymusic = loadSound("assets/sounds/17. Haunted Fortress.mp3")
+}
+
+
+
+
 /**
  * Creates the canvas and initializes the fly
  */
@@ -51,16 +114,26 @@ function setup() {
 
     // Give the fly its first random position
     resetFly();
+
+    sounds.buzzing.loop();
+    sounds.beebuzzing.loop();
+    sounds.scarymusic.loop();
 }
 
 function draw() {
-    background("#87ceeb");
+    background("#662222ff");
     moveFly();
     drawFly();
+    moveirregularfly();
+    drawirregularfly();
+    moveslowfly();
+    drawslowfly();
     moveFrog();
     moveTongue();
     drawFrog();
     checkTongueFlyOverlap();
+    checkTongueirregularflyOverlap();
+    checkTongueslowflyOverlap();
 }
 
 /**
@@ -76,6 +149,25 @@ function moveFly() {
     }
 }
 
+function moveirregularfly() {
+    // Move the fly
+    irregularfly.x += irregularfly.speed
+    // Handle the fly going off the canvas
+    if (irregularfly.x > width) {
+        resetirregularfly();
+    }
+}
+
+function moveslowfly() {
+    // Move the fly
+    slowfly.x += noise(0.5, 10, 100);
+
+    // Handle the fly going off the canvas
+    if (slowfly.x > width) {
+        resetslowfly();
+    }
+}
+
 /**
  * Draws the fly as a black circle
  */
@@ -87,6 +179,22 @@ function drawFly() {
     pop();
 }
 
+function drawirregularfly() {
+    push();
+    noStroke();
+    fill("#f0ff00");
+    ellipse(irregularfly.x, irregularfly.y, irregularfly.size);
+    pop();
+}
+
+function drawslowfly() {
+    push();
+    noStroke();
+    fill("#ff00ff");
+    ellipse(slowfly.x, slowfly.y, slowfly.size);
+    pop();
+}
+
 /**
  * Resets the fly to the left with a random y
  */
@@ -95,11 +203,21 @@ function resetFly() {
     fly.y = random(0, 300);
 }
 
+function resetirregularfly() {
+    irregularfly.x = 0;
+    irregularfly.y = random(0, 300);
+}
+
+function resetslowfly() {
+    slowfly.x = 0;
+    slowfly.y = random(0, 300);
+}
+
 /**
  * Moves the frog to the mouse position on x
  */
 function moveFrog() {
-    frog.body.x = mouseX;
+    frog.body.x
 }
 
 /**
@@ -136,23 +254,48 @@ function moveTongue() {
 function drawFrog() {
     // Draw the tongue tip
     push();
-    fill("#ff0000");
+    fill("#b869a0ff");
     noStroke();
     ellipse(frog.tongue.x, frog.tongue.y, frog.tongue.size);
     pop();
 
     // Draw the rest of the tongue
     push();
-    stroke("#ff0000");
+    stroke("#b869a0ff");
     strokeWeight(frog.tongue.size);
     line(frog.tongue.x, frog.tongue.y, frog.body.x, frog.body.y);
     pop();
 
     // Draw the frog's body
     push();
-    fill("#00ff00");
+    fill("#09ff00ff");
     noStroke();
     ellipse(frog.body.x, frog.body.y, frog.body.size);
+    pop();
+
+    //frog eyes 
+    push();
+    fill("#000000")
+    noStroke();
+    ellipse(frog.Eye1.x, frog.Eye1.y, frog.Eye1.size);
+    pop();
+
+    push();
+    fill("#000000")
+    noStroke();
+    ellipse(frog.Eye2.x, frog.Eye2.y, frog.Eye2.size);
+    pop();
+
+    push();
+    fill("#ff0000ff")
+    noStroke();
+    ellipse(frog.Pupil1.x, frog.Pupil1.y, frog.Pupil1.size);
+    pop();
+
+    push();
+    fill("#ff0000ff")
+    noStroke();
+    ellipse(frog.Pupil2.x, frog.Pupil2.y, frog.Pupil2.size);
     pop();
 }
 
@@ -165,8 +308,40 @@ function checkTongueFlyOverlap() {
     // Check if it's an overlap
     const eaten = (d < frog.tongue.size / 2 + fly.size / 2);
     if (eaten) {
+        sounds.gulp.play();
+        sounds.scream.play();
         // Reset the fly
         resetFly();
+        // Bring back the tongue
+        frog.tongue.state = "inbound";
+    }
+}
+
+function checkTongueirregularflyOverlap() {
+    // Get distance from tongue to fly
+    const d = dist(frog.tongue.x, frog.tongue.y, irregularfly.x, irregularfly.y);
+    // Check if it's an overlap
+    const eaten = (d < frog.tongue.size / 2 + irregularfly.size / 2);
+    if (eaten) {
+        sounds.gulp.play();
+        sounds.anotherscream.play();
+        // Reset the fly
+        resetirregularfly();
+        // Bring back the tongue
+        frog.tongue.state = "inbound";
+    }
+}
+
+function checkTongueslowflyOverlap() {
+    // Get distance from tongue to fly
+    const d = dist(frog.tongue.x, frog.tongue.y, slowfly.x, slowfly.y);
+    // Check if it's an overlap
+    const eaten = (d < frog.tongue.size / 2 + slowfly.size / 2);
+    if (eaten) {
+        sounds.gulp.play();
+        sounds.otherscream.play();
+        // Reset the fly
+        resetslowfly();
         // Bring back the tongue
         frog.tongue.state = "inbound";
     }
@@ -178,5 +353,6 @@ function checkTongueFlyOverlap() {
 function mousePressed() {
     if (frog.tongue.state === "idle") {
         frog.tongue.state = "outbound";
+        sounds.slurp.play();
     }
 }

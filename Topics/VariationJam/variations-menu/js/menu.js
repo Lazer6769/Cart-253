233c83f3b94fe95bb005060a7506036ff5f0d3b1
Lazer6769 <Laser197@hotmail.com>
2025-11-30 15:26:32 -0500

    added variation menus as well as website for cart211

---
+++ Topics/VariationJam/variations-menu/js/menu.js
@@ -0,0 +1,50 @@
+/**
+ * This menu file contains the code to run *only* the menu part of the program.
+ * Note how it has its own draw, menuDraw(), and its own keyPressed, menuKeyPressed().
+ * This keeps the stuff the menu needs to do *separate* from the rest of the program.
+ */
+
+const menuText = `
+(R) Red variation
+(G) Green variation
+(B) Blue variation`
+
+/**
+ * Display the main menu
+ */
+function menuDraw() {
+    background(0);
+
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(menuText, width / 2, height / 2);
+    pop();
+}
+
+/**
+ * Listen to the keyboard
+ */
+function menuKeyPressed(event) {
+    switch (event.keyCode) {
+        case 82:
+            window.open("https://lazer6769.github.io/Cart-253/Topics/VariationJam/pumpkinsmasher/");
+            break;
+
+        case 71:
+            window.open("https://lazer6769.github.io/Cart-253/Topics/VariationJam/regularfrog/");
+            break;
+
+        case 66:
+            window.open("https://lazer6769.github.io/Cart-253/Topics/VariationJam/frogheaven/");
+            break;
+    }
+}
+
+/**
+ * This will be called whenever the mouse is pressed while the menu is active
+ */
+function menuMousePressed() {
+
+}

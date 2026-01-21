358469169a86d5f8dacc454076de7ee9ccb781d5
Lazer6769 <Laser197@hotmail.com>
2026-01-21 18:18:12 -0500

    something changed

--- Topics/Cart-263/assingment1_263/js/qusix.js
+++ Topics/Cart-263/assingment1_263/js/qusix.js
@@ -7,17 +7,17 @@ function draw() {
     background(0)
     textSize(28)
     fill(255)
-    text('test', 200, 200)
+    text('test', 167, 200)
 
     // Loop 10 times with x offset
     for (let i = 0; i < 10; i++) {
-        textSize(12)
+        textSize(14);
         text(i, i * 30, 10);
     }
 
     // Loop 15 times (15 down to 1) with y offset
     for (let i = 15; i >= 1; i--) {
-        text(i, 0, i * 25);
+        text(i, 0, i * 26);
     }
 
 }
